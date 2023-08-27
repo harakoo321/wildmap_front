32a7a8200136badd7c16e3af9ac364bdb2fff171
harakoo321 <kouseih321@gmail.com>
2023-08-27 16:51:38 +0900

    現在地から指定した範囲内のピンが見えるように変更 change:addContribution.js 位置情報のハッシュ値を保存できるように変更 change:getContribution.js ハッシュ値から範囲を指定できるように変更

--- pages/api/addContribution.js
+++ pages/api/addContribution.js
@@ -1,16 +1,19 @@
 import { db } from "@/helpers/firebase";
 import { addDoc, collection } from "firebase/firestore";
 import { GeoPoint } from "firebase/firestore";
+import { geohashForLocation } from "geofire-common";
 
 export const addContribution = async({ name, position, dangerLevel, imagePath, comment }) => {
     try {
+        const hash = geohashForLocation(position);
         const docRef = await addDoc(collection(db, "contribution"), {
             name: name,
             position: position,
             dangerLevel: dangerLevel,
             imagePath: imagePath,
             comment: comment,
-            geoPoint: new GeoPoint(position[0], position[1])
+            geoPoint: new GeoPoint(position[0], position[1]),
+            geoHash: hash
         });
       
         console.log("Document written with ID: ", docRef.id);
--- pages/api/getContribution.js
+++ pages/api/getContribution.js
@@ -1,14 +1,26 @@
-import { collection, getDocs, where, query,  } from "firebase/firestore"; 
+import { collection, getDocs, where, query, orderBy, startAt, endAt } from "firebase/firestore"; 
 import { db } from "@/helpers/firebase";
+import { geohashQueryBounds } from "geofire-common";
 
 export const getContribution = async(center) => {
     try {
-        const q = query(collection(db, "contribution"), 
-            where('position', '>=', [center.lat - 0.05, center.lng - 0.05]), 
-            where('position', '<=', [center.lat + 0.05, center.lng + 0.05])
-        );
-        const querySnapshot = await getDocs(q);
-        return querySnapshot;
+        //10km以内
+        const radiusInM = 10 * 1000;
+        const bounds = geohashQueryBounds([center.lat, center.lng], radiusInM);
+        const promises = [];
+        for (const b of bounds) {
+        const q = query(
+            collection(db, "contribution"), 
+            orderBy("geoHash"), 
+            startAt(b[0]), 
+            endAt(b[1]));
+
+            promises.push(getDocs(q));
+        }
+
+        const querySnapshot = await Promise.all(promises);
+        console.log(querySnapshot);
+        return querySnapshot[0];
     } catch (e) {
         console.error("Error getting document: ", e);
     }
